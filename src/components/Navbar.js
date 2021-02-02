f79d7958cd458c7c4ce0be531ac4f421921e1de0
seanshandy <muhamadseanshandy@gmail.com>
2021-02-02 11:45:01 +0700

    add navbar and logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect} from 'react';
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+import { IconContext} from 'react-icons/lib';
+import { ReactComponent as Logo } from '../images/icon_pokeball.svg';
+
+
+function Navbar() {
+    const [click, setClick]  = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+
+    return (
+        <>
+        <IconContext.Provider value={{ color: '#fff'}}>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link className="navbar-logo" to='/' onClick={closeMobileMenu}>
+                        <Logo className="nav-icon" />
+                        Pokemon
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick} >
+                        {click ? <FaTimes /> : <FaBars />}
+                    </div>
+                    <ul className={click ? 'nav-menu active': 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                                Pokemon List
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/my-pokemon-list' className="nav-links" onClick={closeMobileMenu}>
+                                My Pokemon List
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/pokemon-detail' className="nav-links" onClick={closeMobileMenu}>
+                                Pokemon Detail
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar
